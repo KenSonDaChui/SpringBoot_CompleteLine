513d645b45805ba700a2a2b5f16ef42f56819f77
xutongxue <songhu_kenson@163.com>
2020-10-10 16:57:44 +0800

    ES导入数据脚本

--- src/main/java/com/jiucheng/service/DataToEsService.java
+++ src/main/java/com/jiucheng/service/DataToEsService.java
@@ -11,8 +11,6 @@ import org.frameworkset.tran.task.TaskCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
